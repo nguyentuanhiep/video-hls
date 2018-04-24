f59aab40858323ec89c4b06a82f8872f60a23cdc
nguyentuanhiep <nthiep.2508@gmail.com>
2018-04-24 18:12:39 +0700

    update

--- src/scripts/list.js
+++ src/scripts/list.js
@@ -1,5 +1,32 @@
 Vue.component('components-list', {
-    template: '<div><a href="video1.html"><div class="list-video"><div class="left"><img src="./audio/img/ronaldo.jpg"></div><div class="right"><p>video.mp4</p></div></div></a><a href="video2.html"><div class="list-video"><div class="left"></div><div class="right"><p>mov_bbb.mp4</p></div></div></a><a href="video3.html"><div class="list-video"><div class="left"></div><div class="right"><p>output_dash.mp4</p></div></div></a></div>',
+    template: 
+        `<a href="video1.html">
+            <div class="list-video">
+                <div class="left">
+                    <img src="./audio/img/ronaldo.jpg">
+                </div>
+                <div class="right">
+                    <p>video.mp4</p>
+                </div>
+            </div>
+        </a>
+        <a href="video2.html">
+            <div class="list-video">
+                <div class="left"></div>
+                <div class="right">
+                    <p>mov_bbb.mp4</p>
+                </div>
+            </div>
+        </a>
+        <a href="video3.html">
+            <div class="list-video">
+                <div class="left"></div>
+                <div class="right">
+                    <p>output_dash.mp4</p>
+                </div>
+            </div>
+        </a>`
+    
 })
 
 
